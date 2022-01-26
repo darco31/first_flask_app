6c0e85bff8ec3c30b8184c3e09341ae19bb7d948
Stephen <sdarcy29@hotmail.com>
2022-01-26 20:24:39 +0000

    Add bootstrap and link to bootstrap

--- app.py
+++ app.py
@@ -1,7 +1,46 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def  index():
-    return "Hello World"
+    """
+    First route to define a vue to the home page
+    """
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    """
+    Second route to define a vue to the about page
+    """
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    """
+    Third route to define a vue to the contact page
+    """
+    return render_template("contact.html")
+
+
+@app.route("/careers")
+def careers():
+    """
+    Third route to define a vue to the contact page
+    """
+    return render_template("careers.html")
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True
+    )
+    
